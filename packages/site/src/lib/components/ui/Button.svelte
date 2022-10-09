7a088a561c2f0217e2802f11e326acaf2906d296
Ben Riazy <hamon.riazy@gmail.com>
2022-10-08 21:52:56 -0500

    feat: Push notifications

--- packages/site/src/lib/components/ui/Button.svelte
+++ packages/site/src/lib/components/ui/Button.svelte
@@ -5,11 +5,15 @@
   const dispatch = createEventDispatcher();
   export let loading = false;
   export let title: 'Get Started';
+  export let secondary = false;
+
+  const primaryClasses = "relative inline-flex items-center place-items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+  const secondaryClasses = "inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 
 </script>
 
 <div class="">
-  <button class="relative inline-flex items-center place-items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+  <button class={secondary ? secondaryClasses : primaryClasses}
           on:click><div class:opacity-0={loading}>{title}</div><div class:opacity-0={!loading} class="absolute loader"></div></button>
 </div>
 
--- packages/site/src/routes/+page.svelte
+++ packages/site/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  
+
 </script>
 
 <div class="mx-auto max-w-5xl py-16 px-4 sm:px-6 lg:px-8">
@@ -8,14 +8,14 @@
       <div class="mx-auto max-w-7xl py-12 px-8 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-24 lg:px-8">
         <h2 class="text-3xl font-bold tracking-tight text-gray-900 md:text-4xl">
           <span class="block">Welcome to Orchid Wallet</span>
-          <span class="block text-indigo-600">Create a new Smart Wallet or Open an existing one.</span>
+          <span class="block text-indigo-600">Create a new Smart Wallet or Load an existing one.</span>
         </h2>
         <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
           <div class="inline-flex rounded-md shadow">
             <a href="/create" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700">Create New Wallet</a>
           </div>
           <div class="ml-3 inline-flex">
-            <a href="/load" class="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600 hover:bg-indigo-50">Load Existing Wallet</a>
+            <a href="/load" class="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Load Existing Wallet</a>
           </div>
         </div>
       </div>
